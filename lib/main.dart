12a65e8e7051e0ab94ac452b4a8e08108b53981e
Frastyawan Nym <frastyawan.nym@gmail.com>
2020-10-09 15:53:05 +0800

    Styling & Theming the App

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Shop App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
+        accentColor: Colors.deepOrange,
+        fontFamily: "Lato",
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: ProductsOverviewScreen(),
