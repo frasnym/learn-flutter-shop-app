08516a82cf7e9f13ae7fdb88996b94fcbac74656
Frastyawan Nym <frastyawan.nym@gmail.com>
2020-10-13 12:10:36 +0800

    Edit Product Screen

---
+++ lib/screens/edit_product_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class EditProductProduct extends StatefulWidget {
+  @override
+  _EditProductProductState createState() => _EditProductProductState();
+}
+
+class _EditProductProductState extends State<EditProductProduct> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Edit Product"),
+      ),
+    );
+  }
+}
