195f94e006373be2ee094b957b5e30a938866db3
Frastyawan Nym <frastyawan.nym@gmail.com>
2020-10-14 14:33:19 +0800

    Creating Custom Exceptions & More Error Handling

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+import '../models/http_exception.dart';
 import './product.dart';
 
 class Products with ChangeNotifier {
@@ -143,21 +144,20 @@ class Products with ChangeNotifier {
     }
   }
 
-  void deleteProduct(String id) {
-    final url = "https://learn-flutter-2be71.firebaseio.com/products/$id";
+  Future<void> deleteProduct(String id) async {
+    final url = "https://learn-flutter-2be71.firebaseio.com/products/$id.json";
     final existingProductIndex = _items.indexWhere((prod) => prod.id == id);
     var existingProduct = _items[existingProductIndex];
 
-    http.delete(url).then((response) {
-      if (response.statusCode >= 400) {
-        
-      }
-      existingProduct = null;
-    }).catchError((_) {
-      _items.insert(existingProductIndex, existingProduct);
-    });
-
     _items.removeAt(existingProductIndex);
     notifyListeners();
+
+    final response = await http.delete(url);
+    if (response.statusCode >= 400) {
+      _items.insert(existingProductIndex, existingProduct);
+      notifyListeners();
+      throw HttpException("Could not delete product.");
+    }
+    existingProduct = null;
   }
 }
